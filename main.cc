c6e615608f2d343c1d510779fe93a4764825cef4
kcy1019 <gkcy1019@gmail.com>
2015-03-26 00:05:46 +0900

    Added an option(-z|--zoom), and script output feature.

--- main.cc
+++ main.cc
@@ -20,6 +20,7 @@ WINDOW* InitCurses(void)
 	WINDOW *ret = initscr();
 	getmaxyx(ret, lines, cols);
 	nodelay(ret, TRUE);
+	keypad(stdscr, TRUE);
 	timeout(0);
 	start_color();
 	for (int i = 0; i < COLORS; i++)
@@ -27,25 +28,69 @@ WINDOW* InitCurses(void)
 	return ret;
 }
 
+const int STDOUT(1);
 int main(int argc, char *argv[])
 {
-	InitCurses();
-	if (argc < 2) return puts("usage: ./img2term <filename>"), endwin(), 0;
+	const char usage[] =
+		"[38;5;11mimg2term - convert image into terminal ascii art.[38;5;15m\n"
+		"usage1: img2term (filename|url) [38;5;14m[-z|--zoom] [38;5;10m# print to screen[38;5;15m\n"
+		"usage2: img2term (filename|url) [38;5;9m> my_img.sh [38;5;10m# convert the image into shell script\n"
+		"        the script can be used by typing bash my_img.sh[38;5;7m";
 
+	if (argc < 2) {
+		puts(usage);
+		return 0;
+	}
+
+	bool is_zoom_mode = argc > 2 &&
+						((string)argv[2] == "-z" ||
+						 (string)argv[2] == "--zoom");
+
+	InitCurses();
 	ImageLoader *loader = ImageLoader::get_instance(argv[0]);
 	vector<vector<vector<uint32_t>>> img;;
 	vector<uint32_t> delay;
-	tie(delay, img) = loader->load(argv[1], cols/2, lines);
-	vector<vector<string>> asc = IMG2ASCII(img).convert_raw();
+
+	if (!is_zoom_mode) {
+		tie(delay, img) = loader->load(argv[1], cols/2, lines);
+	} else {
+		tie(delay, img) = loader->load(argv[1], cols/2, 1 << 16);
+	}
+
+	if (!isatty(STDOUT)) {
+		/* stdout is redirected to non-terminal output.
+		 * print as bash script format */
+		vector<vector<string>> asc = IMG2ASCII(img).convert();
+		uint32_t fptr = 0;
+		printf("export TERM=xterm-256color\n");
+		printf("clear\n");
+		for (auto& frm: asc) {
+			uint32_t rptr = 1;
+			for (auto& row: frm) {
+				printf("echo \"[%d;1H\"\n", rptr);
+				printf("echo \"%s\"\n", row.c_str());
+				++rptr;
+			}
+			printf("sleep %.2f\n", delay[fptr] / 100.);
+			++fptr;
+		}
+		clear();
+		refresh();
+		endwin();
+		return 0;
+	}
 
 	clear();
 	refresh();
 
+	vector<vector<string>> asc = IMG2ASCII(img).convert_raw();
+	int zoom_ptr = 0;
 	for (;;) {
 		uint32_t fptr = 0;
 		for (auto& frm: asc) {
 			int rptr = 0, cptr = 0;
-			for (auto& row: frm) {
+			for (int r = zoom_ptr; r < frm.size(); r++) {
+				auto& row = frm[r];
 				for (cptr = 0; cptr < row.size(); cptr++) {
 					int col = (unsigned char)row[cptr];
 					attrset(COLOR_PAIR(col));
@@ -53,13 +98,23 @@ int main(int argc, char *argv[])
 				}
 				++rptr;
 			}
-			mvprintw(rptr, 0, "Press q(Q) to quit");
+			attrset(COLOR_PAIR(0));
+			mvprintw(min(rptr, lines-1),  0, "Press q(Q) to quit");
+			mvprintw(min(rptr, lines-1), 18, ", and press arrow keys to move");
 			refresh();
 			usleep(10000 * delay[fptr]);
 			++fptr;
 		}
-		char ch = getch();
-		if (ch == 'q' || ch == 'Q') break;
+		int ch = getch();
+		if (ch == KEY_DOWN) {
+			if (-zoom_ptr + (int)asc[0].size() > lines)
+				++zoom_ptr;
+		} else if (ch == KEY_UP) {
+			if (zoom_ptr > 0)
+				--zoom_ptr;
+		} else if (ch == 'q' || ch == 'Q') {
+			break;
+		}
 	}
 
 	endwin();
